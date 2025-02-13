4ababb35e8085c254807d27788d21a392c57b73a
izazakuro <izazakuro@gmail.com>
2025-02-13 22:55:53 +0900

    Kubernetes Deployment

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -14,8 +14,8 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 8081")
-	err := http.ListenAndServe(":8081", nil)
+	fmt.Println("Starting front end service on port 8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -53,6 +53,7 @@ func render(w http.ResponseWriter, t string) {
 	if data.BrokerURL == "" {
 		data.BrokerURL = "http://backend"
 	}
+	// data.BrokerURL = "http://localhost:8080"
 
 	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
